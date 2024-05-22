96508ee055ac9da68c60c72413db6968b4446e5d
ahmetoguzhanengin <ahmetoguzhanengin@gmail.com>
2024-05-22 14:12:57 +0300

    form ekranları düzeltildi. Crispy form eklendi

--- blog/settings.py
+++ blog/settings.py
@@ -33,13 +33,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #DJANGO UYGULAMALARI
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #KENDİ UYGULAMALARIM
     'post',
+    #3.PARTİ UYGULAMALAR
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +134,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
