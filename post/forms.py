363859b801050be598b1476b9a480824f668e54b
ahmetoguzhanengin <ahmetoguzhanengin@gmail.com>
2024-05-23 18:23:11 +0300

    Yorum ekleme sayfası yapldı

--- post/forms.py
+++ post/forms.py
@@ -1,5 +1,6 @@
 from django import  forms
 from .models import Post
+from .models import Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -8,4 +9,12 @@ class PostForm(forms.ModelForm):
             'title',
             'content',
             'image',
+        ]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = [
+            'name',
+            'content',
         ]
